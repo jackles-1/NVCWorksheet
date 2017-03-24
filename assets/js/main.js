0042cf5fbef45c73f24e207602b442836883e87d
Jackie Stonebraker <sunktanka@gmail.com>
2017-03-24 17:39:08 -0600

    add buttons to feelings selection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+// Initializes drop downs (so that they can be selected from)
 $('.ui.dropdown')
   .dropdown();
 
+$(document).on("click", ".ui.dropdown", function(e){
+	alert($(this).find("["))
+});
