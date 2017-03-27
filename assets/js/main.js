43c34b64cda8474145b4ccc74c8acc841a3ce002
Jackie Stonebraker <sunktanka@gmail.com>
2017-03-27 17:39:14 -0600

    feelings grid
    -set up grid to organize feelings

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,4 @@
 // Initializes drop downs (so that they can be selected from)
-$('.ui.dropdown')
-  .dropdown();
+// $('.ui.dropdown')
+//   .dropdown();
 
-$(document).on("click", ".ui.dropdown", function(e){
-	alert($(this).find("["))
-});
