24a578856974acea8c3268b7b05226667ec6fe99
Jackie <sunktanka@gmail.com>
2017-04-05 23:28:35 +0000

    add ability to select feelings
    -add ability to check a feeling, and have it add to the final statement
    -add ability to uncheck a feeling, and have it remove from the final statement

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,23 @@
+var finalStatement;
+var feelingsArray = [];
+var feelingsStatement;
+var numFeelings = 0;
+
 // Select Button
 $("button.selectFeeling").on("click", function(){
+   var word = $(this).data("word");
+   
    $(this).find("i").toggleClass("radio") ;
-   $(this).find("i").toggleClass("check circle outline");
+   if($(this).find("i").hasClass("check")){
+      $(this).find("i").removeClass("check circle outline")
+      console.log("remove");
+      updateFeelings(word, false);      
+   }
+   else{
+      $(this).find("i").addClass("check circle outline")
+      console.log("add");
+      updateFeelings(word, true)
+   }
 });
 
 // Definition Button
@@ -9,9 +25,45 @@ $("button.defineFeeling").on("click", function(){
    var word = $(this).data("word");
    var definition = $(this).data("definition");
    var partOfSpeech = $(this).data("partofspeech");
+   
    $("div.ui.modal div.header").text(word);
    $("#definition").html(definition);
    $("#partOfSpeech").text(partOfSpeech);
    $(".ui.modal").modal("show");   
 });
+
+
+function updateFeelings(word, add){
+   // Update feelingsArray
+   if(add === true){
+      feelingsArray.push(word);
+   }
+   else{
+      var indexOfWord = feelingsArray.indexOf(word);
+      feelingsArray.splice(indexOfWord, 1);
+   }
+   
+   // Update feelingsStatement
+   if(feelingsArray.length === 1){
+      feelingsStatement = "I feel " + feelingsArray[0];
+   }
+   else if(feelingsArray.length === 2){
+      feelingsStatement = "I feel " + feelingsArray[0] + ", and " + feelingsArray[1];
+   }
+   else if(feelingsArray.length > 2){
+      feelingsStatement = "I feel ";
+      for(i = 0; i < (feelingsArray.length - 2); i++){
+         feelingsStatement += feelingsArray[i] + ", ";
+      }
+      feelingsStatement += feelingsArray[feelingsArray.length - 2] + ", and " + feelingsArray[feelingsArray.length - 1];
+   }
+   
+   updateFinalStatement();
+};
+
+function updateFinalStatement(){
+   finalStatement = feelingsStatement;
+   $("#finalStatement").text(finalStatement);
+};
+
 
