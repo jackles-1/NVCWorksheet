067df560aee4f63269b706f1ac836e7fa081bade
Jackie <sunktanka@gmail.com>
2017-05-04 00:16:13 +0000

    Oxford dictionary API work.
    -Add in oxford-dictionary-api package and set up.

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ var express = require("express"),
 	mongoose = require("mongoose"),
 	expressSanitizer = require("express-sanitizer"),
 	request = require("request"),
-	curl = require("curl"),
+	Dictionary = require("oxford-dictionary-api"),
 // Models
 	Feeling = require("./models/feeling"),
 	seedDB = require("./seeds");
+	
+var qs = require("querystring"),
+	oauth = {
+		app_id: "27cbefc7",
+		app_key: "28e729879e8883e9f98656fc88430520"
+	};
 
 mongoose.connect("mongodb://localhost/nvc");
 
@@ -19,6 +25,10 @@ app.use(express.static(__dirname + "/public"));
 app.use(expressSanitizer());
 
 app.set("view engine", "ejs");
+
+var app_id = "27cbefc7";
+var app_key = "28e729879e8883e9f98656fc88430520";
+var dict = new Dictionary(app_id, app_key);
 
 seedDB();
 
@@ -31,27 +41,27 @@ app.get("/", function(req, res){
 			console.log("Error in root route.");
 		}	
 		else{
-			if(req.xhr){
-				console.log("In xhr");
-				request("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/squirrel/definitions", function(error, response, body){
-					if(error){
-					      console.log(error);
-					  }
-					  else{
-					      console.log("in else outside of if");
-					      if(response.statusCode == 200){
-					         var parsedData = JSON.parse(body);
-					         console.log(req.body.data);
-					         console.log(parsedData["results"]["lexicalEntries"]["senses"]["definitions"]);
-					         console.log("here");
-					      }
-					  }
-					});
-			}
-			else{
-				console.log("In else");
+			// if(req.xhr){
+			// 	console.log("In xhr");
+			// 	request("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/squirrel/definitions", function(error, response, body){
+			// 		if(error){
+			// 		      console.log(error);
+			// 		  }
+			// 		  else{
+			// 		      console.log("in else outside of if");
+			// 		      if(response.statusCode == 200){
+			// 		         var parsedData = JSON.parse(body);
+			// 		         console.log(req.body.data);
+			// 		         console.log(parsedData["results"]["lexicalEntries"]["senses"]["definitions"]);
+			// 		         console.log("here");
+			// 		      }
+			// 		  }
+			// 		});
+			// }
+			// else{
+			// 	console.log("In else");
 				res.render("index", {feelings: feelings});
-			}
+			//}
 		}
 	});
 });
@@ -60,25 +70,52 @@ app.get("/test", function(req, res){
 	res.render("testForm");
 });
 
-app.get("/return", function(req, res){
+app.get("/results", function(req, res){
     var word = req.query.word;
     var url = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + word + "/definitions";
-
-	// curl.getJSON();
-
+    
+    dict.find(word, function(err, data){
+    	console.log(word);
+    	if(err){
+    		console.log("err: " + err);
+    	}	
+    	else{
+    		console.log("data: " + data);
+			res.render("testResults", {data: data});
+		}
+    });
+    
+    
+    
+    
+   // request.post({url:url, oauth:oauth}, function (e, r, body) {
+   // // ready to make signed requests on behalf of the user 
+   // var perm_data = qs.parse(body)
+   //   , oauth =
+   //     {		
+   //     	app_id: "27cbefc7",
+			// app_key: "28e729879e8883e9f98656fc88430520"
+   //     }
+   //   , url = 'https://api.twitter.com/1.1/users/show.json'
+   //   , qs =
+   //     { screen_name: perm_data.screen_name
+   //     , user_id: perm_data.user_id
+   //     }
+   // });
+    
+   // request.get({url:url, oauth:oauth, qs:qs, json:true}, function(e, r, response){
+   // 	console.log(response);	
+   // });
+    
     // request(url, function(error, response, body){
-    //   if(!error && response.statusCode == 200){
-  		// console.log("HERE");
-    // 	var data = JSON.parse(body);
-    // 	res.render("testResults");
-    //   } 
-    // });
+    // 	if(!error && response.statusCode == 200){
+	  	// 	console.log("HERE");
+	   // 	var data = JSON.parse(body);
+	   // 	res.render("testResults");		
+    // 	}
+    //});
+
 });
-
-// curl.getJSON(url, options, function(err, response, data){});
-
-// curl -X GET --header "Accept: application/json" --header "app_id: 27cbefc7" --header "app_key: 28e729879e8883e9f98656fc88430520" 
-// "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace"
 
 // Listener
 app.listen(process.env.PORT, process.env.IP, function(){
